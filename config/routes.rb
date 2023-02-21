30aac54f50b7e0f8bad2d17925ca9e7245631812
Irirwanirira <irijoseph41@gmail.com>
2023-02-21 20:00:45 +0200

    Style devise

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :starts, only: %i[index]
-  root 'start#index'
+  root 'starts#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
