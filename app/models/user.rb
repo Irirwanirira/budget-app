7d27bbef7e85d80cf512eba90a72e49943597e07
Irirwanirira <irijoseph41@gmail.com>
2023-02-21 10:33:26 +0200

    Validate user, create transactions and categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  has_many :categories
+  has_many :transactions
+  validates :name, presence:true, length: {in: 1..20}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
