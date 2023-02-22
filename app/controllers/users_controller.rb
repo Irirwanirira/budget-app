c6d06aa2cbffb16f655fdd3682cc85f2304cb97c
Irirwanirira <irijoseph41@gmail.com>
2023-02-22 16:28:48 +0200

    style categories and transactions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,8 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = current_user
   end
 
-  # GET /users/1 or /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
 end
 
