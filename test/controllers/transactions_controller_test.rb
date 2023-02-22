17c5d5b5745df1d4d0dc55a2cf0e006fa39d6f4b
Irirwanirira <irijoseph41@gmail.com>
2023-02-23 01:03:57 +0200

    Fix linters

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -18,8 +18,9 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
   test 'should create transaction' do
     assert_difference('Transaction.count') do
       post transactions_url,
-           params: { transaction: { amount: @transaction.amount, category_id: @transaction.category_id, name: @transaction.name,
-           user_id: @transaction.user_id } }
+           params: { transaction: { amount: @transaction.amount,
+                                    category_id: @transaction.category_id, name: @transaction.name,
+                                    user_id: @transaction.user_id } }
     end
 
     assert_redirected_to transaction_url(Transaction.last)
@@ -37,8 +38,9 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should update transaction' do
     patch transaction_url(@transaction),
-          params: { transaction: { amount: @transaction.amount, category_id: @transaction.category_id, name: @transaction.name,
-          user_id: @transaction.user_id } }
+          params: { transaction: { amount: @transaction.amount,
+                                   category_id: @transaction.category_id, name: @transaction.name,
+                                   user_id: @transaction.user_id } }
     assert_redirected_to transaction_url(@transaction)
   end
 
